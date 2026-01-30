0206c01d7ad666a10cc5442cef7daff8f6d54a84
Juan David Velasquez <jdvelasqu2@eafit.edu.co>
2026-01-29 19:38:34 -0500

    Durer renderer

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using SkiaSharp;
 
 public static class Program
 {
@@ -12,15 +13,54 @@ public static class Program
 
     private static void Render(Shape2D shape, RenderParameters parameters, string outputPath)
     {
-        //TODO: Use skiasharp to render the shape to the output path, the test checks for this location
-        // with the given parameters.
-        throw new NotImplementedException();
+        List<SKPoint> coordenadas = new();
+        
+        for (int i=0; i < shape.Points.Length; i++)
+        {
+            float x=(shape.Points[i][0]-parameters.XMin)/(parameters.XMax-parameters.XMin);
+            float y=(shape.Points[i][1]-parameters.YMin)/(parameters.YMax-parameters.YMin);
+            x= x*parameters.Resolution;
+            y=(1-y)*parameters.Resolution;
+            coordenadas.Add(new SKPoint(x,y));
+        };
+
+        using SKBitmap bitmap = new(parameters.Resolution, parameters.Resolution);
+        using SKCanvas canvas = new(bitmap);
+        canvas.Clear(SKColors.White);
+        using SKPaint paint = new()
+        {
+            Color = SKColors.Black,
+            StrokeWidth = 2,
+            IsAntialias = true
+        };
+
+        for (int i=0; i < shape.Lines.Length; i++)
+        {
+            int a = shape.Lines[i][0];
+            int b = shape.Lines[i][1];
+
+            canvas.DrawLine(coordenadas[a], coordenadas[b], paint);
+        };
+        using var image = SKImage.FromBitmap(bitmap);
+        using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+        using var stream = File.OpenWrite(outputPath);
+        data.SaveTo(stream);
+
     }
 
     private static Shape2D ProjectShape(Model3D model)
     {
-        //TODO: Implement projection logic
-        throw new NotImplementedException();
+        float[][] Points=new float[model.VertexTable.Length][];
+        for (int i = 0; i < model.VertexTable.Length; i++)
+        {
+            float X=model.VertexTable[i][0]/model.VertexTable[i][2];
+            float Y=model.VertexTable[i][1]/model.VertexTable[i][2];
+            Points[i]=[X, Y];
+        };
+        Shape2D shape = new Shape2D();
+        shape.Points=Points;
+        shape.Lines=model.EdgeTable;
+        return shape;
     }
 }
 
